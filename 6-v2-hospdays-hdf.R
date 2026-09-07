d7cb03a81e6ba83c6b8d07cec87f0d7fd9c5813e
Sanne Roos <sanne.isabel@gmail.com>
2026-09-07 16:38:44 +0200

    Update 6-v2-hospdays-hdf.R

--- 6-v2-hospdays-hdf.R
+++ 6-v2-hospdays-hdf.R
@@ -1,5 +1,6 @@
 #6.0 add total hospitalization days hdf
 #goal is to add total days of hospitalization BEFORE censoring takes place for HDF clone
+#last updated 07-09-2026
 
 ##load packages----
 pacman::p_load( "rio", #load data
@@ -57,48 +58,12 @@ total_hosp_days_hdf <- event_select_hdf %>%
 
 cohort_hdf_reduced <- left_join(cohort_hdf_reduced, total_hosp_days_hdf, by = "id") #add this variable to the main dataset again
 
+#we expect all hospitalizations to be registered and set them to 0 if they are all missing
 cohort_hdf_reduced <- cohort_hdf_reduced %>%
   group_by(id) %>%
   mutate(
     total_hosp_days = if_else(is.na(total_hosp_days), 0, total_hosp_days) #set missing to zero so it does not get imputed
   )
-
-
-
-
-cohort_hdf_reduced <- cohort_hdf_reduced %>%
-  group_by(id) %>%
-  mutate(
-    max_two_week_period = case_when(
-      cens_time <= 14 ~ 1,
-      cens_time >28 & cens_time < 42 ~ 2,
-      cens_time >= 42 & cens_time < 56 ~ 3,
-      cens_time >= 56 & cens_time < 70 ~ 4,
-      cens_time >= 70 & cens_time < 84 ~5,
-      cens_time >= 84 & cens_time < 90 ~6,
-      cens_time >= 90 ~7
-    ),
-    max_year_period = case_when(
-      cens_time <= 90 ~0,
-      cens_time < 360 & cens_time > 90 ~ 0,
-      cens_time >= 360 & cens_time < 720 ~1,
-      cens_time >= 720 & cens_time <1080 ~ 2,
-      cens_time >= 1080 & cens_time < 1440 ~3,
-      cens_time >= 1440 & cens_time < 1800 ~4,
-      cens_time >= 1800 ~5
-    )
-  )%>%
-  ungroup()
-
-cohort_hdf_reduced <- cohort_hdf_reduced %>%
-     group_by(id) %>%
-     mutate(
-    remove = if_else(two_week_period > max_two_week_period | year_period > max_year_period, 1, 0), #make identifier for extra rows beyond max two week period or year period that should be removed
-    days_from_fdd = days_from_fdd.x #use day identifier from the dialysis data not the lab data
-     ) %>%
-  filter(remove == 0) %>%
-  select(-remove)%>%
-  ungroup()
 
 cohort_hdf_reduced <- cohort_hdf_reduced %>%
   mutate(
@@ -106,4 +71,3 @@ cohort_hdf_reduced <- cohort_hdf_reduced %>%
   )
 
 save(cohort_hdf_reduced, file = paste0(path, "cohort_hdf_reduced.Rdata"))
-
